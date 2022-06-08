51d46a8ef9660a132eee5380a68a0279d9d4900e
Oleksandr Vasin <92584573+deduvoevali@users.noreply.github.com>
2022-06-08 13:30:12 +0300

    Adding file

---
+++ service.js
@@ -0,0 +1,21 @@
+const postData = async (url, data) => {
+    const result = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: data
+    });
+    return await result.json();
+};
+
+const getResource = async (url) => {
+    const result = await fetch(url);
+
+    if (!result.ok) {
+        throw new Error(`Couldn't fetch ${url}, status ${result.status}`);
+    }
+
+    return await result.json();
+};
+
